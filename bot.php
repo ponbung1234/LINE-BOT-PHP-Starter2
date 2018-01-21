31604a884ff59f0d8753a67e237bec8c7a1c98f6
Thanapon <31815206+ponbung1234@users.noreply.github.com>
2018-01-21 15:01:03 +0700

    xx

--- bot.php
+++ bot.php
@@ -6,7 +6,6 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
